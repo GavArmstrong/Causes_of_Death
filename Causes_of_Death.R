e355d0cb78b2b4d0ae4eabaee55813f3eee42eb3
GavArmstrong <gavin.k.armstrong@gmail.com>
2020-09-18 23:15:45 -0600

    Frame of R file

--- Causes_of_Death.R
+++ Causes_of_Death.R
@@ -38,43 +38,18 @@ Counties <- st_as_sf(maps::map("county", plot = FALSE, fill = TRUE)) %>%
   as_tibble()
 
 
-# Join States and data
-States %<>% left_join(Loans)
-
 # Simple plot of State boundaries
 ggplot(USA) +
   geom_sf(fill = "antiquewhite1",
           lwd=0,
           color="black") +
   geom_sf(data=States,
-          aes(geometry=geom,
-              fill=Debt_per_Person),
+          aes(geometry=geom),
           lwd=0.2,
           color="#002240") +
-  scale_fill_gradient(low = "#61ff5e",
-                      high = "#ea1730",
-                      labels=list("$24,000", "$26,000", "$28,000",
-                                  "$30,000", "$32,000", "$34,000")) +
-  ggtitle("Average Federal Student Loan Debt by State (April, 2020)",
-          subtitle = bquote(bold("Source:") ~"https://studentaid.gov/data-center/student/portfolio")) +
-  theme(axis.ticks = element_blank(),
-        axis.text = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.background = element_rect(fill = "#002240"),
-        plot.background = element_rect(fill = "#002240"),
-        legend.text = element_text(color = "white",
-                                   family="Arial",
-                                   size=7),
-        legend.title = element_blank(),
-        legend.key.size = unit(0.55,"cm"),
-        legend.justification=c(1,0), 
-        legend.position=c(0.95, 0.05),  
-        legend.background = element_blank(),
-        plot.title = element_text(color="white",
-                                  family="Arial Rounded MT Bold",
-                                  size=12),
-        plot.subtitle = element_text(color="white",
-                                     family="Arial",
-                                     size=7))
+  geom_sf(data=Counties,
+          aes(geometry=geom),
+          lwd=0.1,
+          color="#4C974C")
 
-ggsave("Debt per Student.png", device="png")
+ggsave("Cause of Death by County.png", device="png")
